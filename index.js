0b42284b18ddf57eff26c24e623128b1681391e3
mostafa ahmed <42159268+Mostafaahmed20@users.noreply.github.com>
2021-02-26 08:32:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const navgation = document.querySelector('.navgationsign')
-const checked = document.querySelector('.checkboxx')
-const firstname = document.getElementById('fname')
-const lastname = document.getElementById('lname')
+// const navgation = document.querySelector('.navgationsign')
+// const checked = document.querySelector('.checkboxx')
+// const firstname = document.getElementById('fname')
+// const lastname = document.getElementById('lname')
 
 
-function one() { 
-    // checked ? navgation.style.disply =  `${-320 } + "px"` : navgation.style.left = 80 + "px"; 
-    if (checked) {
-        navgation.style.left = -320 + "px";
+// function one() { 
+//     // checked ? navgation.style.disply =  `${-320 } + "px"` : navgation.style.left = 80 + "px"; 
+//     if (checked) {
+//         navgation.style.left = -320 + "px";
         
-    } else {
-        navgation.style.left = 80 + "px"; 
-    }
-}
+//     } else {
+//         navgation.style.left = 80 + "px"; 
+//     }
+// }
 
 
 
-function loginuser(fname , lname) {
-    return document.body.innerHTML =  `<h3> ${fname} ${lname} </h3>`
-}
+// function loginuser(fname , lname) {
+//     return document.body.innerHTML =  `<h3> ${fname} ${lname} </h3>`
+// }
 
